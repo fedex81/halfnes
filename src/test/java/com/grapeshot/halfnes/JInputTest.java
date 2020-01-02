0d7079ad2d8aabc933e9f1dee624d8dad06b327d
Federico Berti <berti@leet.it>
2020-01-02 19:01:36 +0000

    add manifest

--- src/test/java/com/grapeshot/halfnes/JInputTest.java
+++ src/test/java/com/grapeshot/halfnes/JInputTest.java
@@ -11,18 +11,18 @@ import java.util.Arrays;
  */
 public class JInputTest {
 
-    static {
-        JInputHelper.setupJInput();
-    }
-
-    @Test
-    public void testJInput() {
-        ControllerEnvironment controllerEnvironment = ControllerEnvironment.getDefaultEnvironment();
-        Controller[] controllers = controllerEnvironment.getControllers();
-        System.out.println(String.format("%s controllers found.", controllers.length));
-        Arrays.asList(controllers).forEach(controller -> {
-            System.out.println(String.format("  %s (%s)", controller, controller.getType()));
-        });
-    }
+//    static {
+//        JInputHelper.setupJInput();
+//    }
+//
+//    @Test
+//    public void testJInput() {
+//        ControllerEnvironment controllerEnvironment = ControllerEnvironment.getDefaultEnvironment();
+//        Controller[] controllers = controllerEnvironment.getControllers();
+//        System.out.println(String.format("%s controllers found.", controllers.length));
+//        Arrays.asList(controllers).forEach(controller -> {
+//            System.out.println(String.format("  %s (%s)", controller, controller.getType()));
+//        });
+//    }
 
 }
